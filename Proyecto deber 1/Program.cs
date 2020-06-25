b125d2c760889bb5fad0501e260e52b7cd5c8a0a
Jean42Medina <40485748+Jean42Medina@users.noreply.github.com>
2020-06-25 03:19:09 -0500

    Add files via upload

---
+++ Proyecto deber 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_deber_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vehiculo carro = new vehiculo();
+            vehiculo.motor = "Apagado";
+            vehiculo.tanqueGasolina = 5;
+            vehiculo.velocidad = 0;
+            carro.encender();
+            Console.WriteLine(vehiculo.motor);
+            carro.aumentar();
+
+            Console.WriteLine(vehiculo.velocidad);
+            carro.disminuir();
+
+            Console.WriteLine(vehiculo.velocidad);
+            carro.frenar();
+
+            Console.WriteLine(vehiculo.velocidad);
+            carro.apagar();
+
+            Console.WriteLine(vehiculo.motor);
+            carro.llenar(4);
+
+            Console.WriteLine(vehiculo.tanqueGasolina);
+            carro.vaciar();
+
+            Console.WriteLine(vehiculo.tanqueGasolina);
+        }
+    }
+}
---
+++ Proyecto deber 1/vehiculo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_deber_1
+{
+    class vehiculo
+    {
+    public static int velocidad;
+    public static String motor;
+    public static int tanqueGasolina;
+    
+    public int aumentar(){
+        if(velocidad >=0 && velocidad <=200){
+            velocidad = velocidad+10;
+            return velocidad;
+        }else{
+            return 200;
+        }
+    }
+    
+    public int disminuir(){
+        if(velocidad >=0){
+            velocidad = velocidad-5;
+        }
+        return velocidad;
+    }
+    
+    public int frenar(){
+        if(velocidad >=0){
+            velocidad =0;
+        }
+        return velocidad;
+    }
+    
+    public String encender(){
+        if(motor=="Apagado"){
+            motor="Encendido";
+        } else { 
+        }
+        return motor;
+    }
+    
+    public String apagar(){
+        if(motor=="Encendido"){
+            motor="Apagado";
+        } else { 
+        }
+        return motor;
+    }
+    
+    public int llenar(int gasolina){
+        if(tanqueGasolina >=0 && tanqueGasolina <=10){
+            tanqueGasolina = tanqueGasolina+gasolina;
+        } else { 
+        }
+        return tanqueGasolina;
+    }
+    
+    public int vaciar(){
+        if(tanqueGasolina > 0){
+            tanqueGasolina = tanqueGasolina-1;
+            return tanqueGasolina;
+        } else {
+            return 0;
+        }
+    }
+
+
+    }
+}
